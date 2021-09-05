b7e2924751b06c1c7a1da1b182f4d380cbce3532
csstndly <679vf56@gmail.com>
2021-09-04 20:44:36 -0400

    Adding the election analysis.

---
+++ .idea/shell/condabin/conda-hook.ps1
@@ -0,0 +1,8 @@
+$Env:CONDA_EXE = "C:/Users/14802/anaconda3\Scripts\conda.exe"
+$Env:_CE_M = ""
+$Env:_CE_CONDA = ""
+$Env:_CONDA_ROOT = "C:/Users/14802/anaconda3"
+$Env:_CONDA_EXE = "C:/Users/14802/anaconda3\Scripts\conda.exe"
+
+Import-Module "$Env:_CONDA_ROOT\shell\condabin\Conda.psm1"
+Add-CondaEnvironmentToPrompt
